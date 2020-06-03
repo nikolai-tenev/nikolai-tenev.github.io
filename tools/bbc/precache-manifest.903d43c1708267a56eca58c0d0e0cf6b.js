446fa6726769259efa99dd5c43479b59d25788ef
bh2o <tenev.bh2o@gmail.com>
2020-06-03 12:15:19 +0300

    1. New version of the Business Budget Calculator.

---
+++ tools/bbc/precache-manifest.903d43c1708267a56eca58c0d0e0cf6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce84e13bcd726ad0dc9f70b4a3ba3d3",
+    "url": "/tools/bbc/index.html"
+  },
+  {
+    "revision": "09c6ac1a8c0eea37cfba",
+    "url": "/tools/bbc/static/js/2.d1005148.chunk.js"
+  },
+  {
+    "revision": "9a37e3909ba434ab55801d39874c868d",
+    "url": "/tools/bbc/static/js/2.d1005148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5a660d976487704de3",
+    "url": "/tools/bbc/static/js/main.44591523.chunk.js"
+  },
+  {
+    "revision": "7fe3e33cabdc50ec0026",
+    "url": "/tools/bbc/static/js/runtime-main.86cf3a33.js"
+  }
+]);
